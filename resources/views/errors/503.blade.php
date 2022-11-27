2b5c5aa30d47c9fe240ce61e65fc97f99769dfbf
Jessica Vides <jess20romero@gmail.com>
2022-11-27 00:22:13 -0600

    pantalla para sistema en mantenimiento

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,11 @@
+@extends('errors.error-base')
+@section('title') 503! @endsection
+@section('h1')
+<i class="fas fa-wrench"></i> Sistema en mantenimiento
+@endsection
+@section('h2')<br>
+El sistema no está disponible temporalmente, <br>Se habilitará lo más pronto posible.
+@endsection
+@section('texto')
+@endsection
+@section('boton') @endsection
