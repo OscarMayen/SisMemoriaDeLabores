15539a6da903d875b34bd872a526103150a2639a
Jessica Vides <jess20romero@gmail.com>
2022-11-27 00:22:02 -0600

    pantallas para error 5XX
    500 y 502 que son los mas comunes

---
+++ resources/views/errors/502.blade.php
@@ -0,0 +1,12 @@
+@extends('errors.error-base')
+@section('title') Error 502! @endsection
+
+@section('h1')
+Error 502!
+@endsection
+@section('h2')
+Puerta de enlace incorrecta.
+@endsection
+@section('texto')
+Si el problema persiste, contacte al administrador del sistema.
+@endsection
