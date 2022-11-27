cec9199343edfc20a88fe939f629aee0b9c18abe
Jessica Vides <jess20romero@gmail.com>
2022-11-27 00:21:32 -0600

    plantillas para los erorres 4xx
    basado en el estandard de los errores mas comunes

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('errors.error-base')
+@section('title') Error 404! @endsection
+
+@section('h1')
+<i class="fa-regular fa-face-sad-tear"></i> 404!
+@endsection
+@section('h2')
+La página que solicita no existe.
+@endsection
+@section('texto')
+@endsection
