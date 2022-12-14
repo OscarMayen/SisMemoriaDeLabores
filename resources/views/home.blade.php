f7427f2e255e265f51c81f7fdc97ba59395e5799
Jessica Vides <jess20romero@gmail.com>
2022-12-14 07:13:36 -0600

    agregando logo al gome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,21 @@
 @section('title','Home')
 
 @section('content_header')
-<h1>Bienvenidos al Sistema para la creación de la Memoria de Labores</h1>
+<h1 class="text-center">Bienvenidos al Sistema para la creación de la Memoria de Labores</h1>
 @stop
     
 @section('content')
 <div class="card">
     <div class="card-header">
-        <h3>Descripción</h3>
+        <h3><i>Descripción</i></h3>
     </div>
 <div class="card-body">
-      <center> <h4>La memoria de labores anual es una herramienta que permite mantener informados a todos los miembros de la comunidad universitaria, 
+    
+      <center> 
+    <div class="content">
+        <h2><img src="vendor/adminlte/dist/img/UESlogo.png"  alt="Cinque Terre" width="167" height="211"></h2><br>
+    </div>
+        <h4>La memoria de labores anual es una herramienta que permite mantener informados a todos los miembros de la comunidad universitaria, 
             en especial a los decanos, vicedecanos y miembros de la Asamblea General Universitaria, 
             sobre las diferentes actividades, proyectos, etc. que se realizan en la institución.</h4>
     </div> </center>  
